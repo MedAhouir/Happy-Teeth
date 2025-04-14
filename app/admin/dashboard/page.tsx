8f1c30e9bda749bcdbc33f85a8a968ea81d6da79
MedAhouir <mohamedahouir@gmail.com>
2025-04-14 18:50:23 +0100

    Edit teeth names

---
+++ app/admin/dashboard/page.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+import { isAdminAuthenticated } from "@/lib/auth";
+import { SidebarDemo } from "@/components/SidebarDemo";
+
+const Home = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!isAdminAuthenticated()) {
+      router.push("/admin");
+    }
+  },);
+
+  return (
+    <section className="h-screen flex flex-col ">
+      <SidebarDemo />
+    </section>
+  );
+};
+
+export default Home;
